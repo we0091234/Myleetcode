afd8ee6eb33b6365ebd0ff34c15274a3c04357d8
we0091234 <447587096@qq.com>
2021-08-27 22:03:53 +0800

    zizichuanuan的

--- 3最长无重复子串.cpp
+++ 3最长无重复子串.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
 #include <string.h>
+#include <unordered_set>
+#include <string.h>
 using namespace std;
-  int lengthOfLongestSubstring(string s)
+  int lengthOfLongestSubstring(string s)  //我自己的实现
    {
        int index1 = 0;
        int flag =0;
@@ -11,23 +13,23 @@ using namespace std;
        int i = 0;
        for( i = 0; i<strlen(sr); i++)
        {
-           for(int j = index1; j<i;j++)
+           for(int j = index1; j<i;j++)  //每增加一个字符，就要和之前的字符进行对比，是否有重复
            {
-                 if(sr[j]==sr[i])
+                 if(sr[j]==sr[i])         //如果有重复的那么标志为1 
                  {
                      flag = 1;
                      break;
                  }
            }
-           if (flag)
+           if (flag)   //代表有重复
            {
-              maxLength = max(maxLength,i-index1);
-              index1=index1+1;
-              i = i-1;
-              flag = 0;
+              maxLength = max(maxLength,i-index1); //那么当前的不重复子串的程度就为i-index1,然后和之前的子串取最大值
+              index1=index1+1;                    //将index=index+1，寻找下一个不重复的子串
+              i = i-1;                // i要向后面退一个
+              flag = 0;           //标志位重新设置为0
            }
        }
-       if(i == strlen(sr) && flag==0)
+       if(i == strlen(sr) && flag==0)       //针对如果找不到重复字符的话，那么flag永远不会为1，那么maxlength就永远为0，所以增加一个判断
        {
            maxLength = max(maxLength,i-index1);
        }
@@ -35,8 +37,31 @@ using namespace std;
        return maxLength;
     }
 
+     int lengthOfLongestSubstring1(string s) //参考他人实现
+     {
+         const char *sr = s.c_str();
+         unordered_set<char >myset;
+         int start=0,end = 0;
+         int maxl =0;
+         while (end <strlen(sr))
+         {
+            if(myset.find(sr[end])!=myset.end())
+            {
+                 myset.erase(myset.find(sr[start]));
+                 start+=1;
+            }
+            else
+            {
+                myset.insert(sr[end++]);
+                if (maxl<myset.size());
+                maxl=myset.size();
+                // maxl=std::max(maxl,myset.size());
+            }
+         }
+         return maxl;
+     }  
 int main(int argc, char ** argv)
 {
-    cout<<lengthOfLongestSubstring("a")<<endl;
+    cout<<lengthOfLongestSubstring("abcbcadfa")<<endl;
     return 0;
 }
